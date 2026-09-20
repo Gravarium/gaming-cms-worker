2af186171c61e2b8f7e59930561bb47f0d434e5e
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-20 18:29:24 +0200

    Use shared password policy for resets

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace App\Form;
 
+use App\Security\PasswordPolicy;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Regex;
 
 /** @extends AbstractType<null> */
 final class ResetPasswordType extends AbstractType
@@ -24,13 +22,7 @@ final class ResetPasswordType extends AbstractType
             'invalid_message' => 'Die beiden Passwörter stimmen nicht überein.',
             'first_options' => ['label' => 'Neues Passwort', 'attr' => ['autocomplete' => 'new-password']],
             'second_options' => ['label' => 'Neues Passwort wiederholen', 'attr' => ['autocomplete' => 'new-password']],
-            'constraints' => [
-                new NotBlank(),
-                new Length(min: 12, max: 4096, minMessage: 'Das Passwort muss mindestens {{ limit }} Zeichen lang sein.'),
-                new Regex(pattern: '/\p{Ll}/u', message: 'Das Passwort braucht mindestens einen Kleinbuchstaben.'),
-                new Regex(pattern: '/\p{Lu}/u', message: 'Das Passwort braucht mindestens einen Großbuchstaben.'),
-                new Regex(pattern: '/\p{N}/u', message: 'Das Passwort braucht mindestens eine Zahl.'),
-            ],
+            'constraints' => PasswordPolicy::constraints(),
         ]);
     }
 
