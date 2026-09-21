b8940aa2c872abe6bbaa39b824463dce4bdab65c
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-21 23:33:43 +0200

    Test category deletion DB invariants

--- tests/Security/SecurityArchitectureTest.php
+++ tests/Security/SecurityArchitectureTest.php
@@ -45,6 +45,18 @@ final class SecurityArchitectureTest extends TestCase
         self::assertStringNotContainsString('$this->credentials->remove($credential)', $source);
     }
 
+    public function testCategoryDeletionIsDatabaseRestrictedAgainstConcurrentReuse(): void
+    {
+        $category = $this->readProjectFile('src/Entity/Category.php');
+        $content = $this->readProjectFile('src/Entity/ContentEntry.php');
+        $migration = $this->readProjectFile('migrations/Version20260921224000.php');
+
+        self::assertStringContainsString("#[ORM\\JoinColumn(onDelete: 'RESTRICT')]", $category);
+        self::assertStringContainsString("#[ORM\\JoinColumn(onDelete: 'RESTRICT')]\n    private ?Category \$category = null;", $content);
+        self::assertStringContainsString('FK_CONTENT_ENTRY_CATEGORY FOREIGN KEY (category_id) REFERENCES content_category (id) ON DELETE RESTRICT', $migration);
+        self::assertStringContainsString('FK_CONTENT_CATEGORY_PARENT FOREIGN KEY (parent_id) REFERENCES content_category (id) ON DELETE RESTRICT', $migration);
+    }
+
     #[DataProvider('adminControllers')]
     public function testEveryAdminControllerHasAnExplicitPermissionBoundary(string $path): void
     {
