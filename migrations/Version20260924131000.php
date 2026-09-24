2066da8905b18e7afc3973601d71686f4ed5651b
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-25 00:01:53 +0200

    FCP-056: give game catalogue migration a unique version

--- migrations/Version20260924131000.php
+++ migrations/Version20260924131000.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260924130000 extends AbstractMigration
+final class Version20260924131000 extends AbstractMigration
 {
     public function getDescription(): string
     {
