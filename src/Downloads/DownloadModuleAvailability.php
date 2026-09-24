cd3bbab1fe5f7b44a8c4a47555893ee21cb88257
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-24 12:28:19 +0200

    FCP-063: wire authorized private download delivery

---
+++ src/Downloads/DownloadModuleAvailability.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace App\Downloads;
+use App\Repository\CmsModuleStateRepository;
+final readonly class DownloadModuleAvailability
+{
+    public function __construct(private CmsModuleStateRepository $states){}
+    public function enabled():bool{return $this->states->find('downloads')?->isEnabled()??true;}
+}
