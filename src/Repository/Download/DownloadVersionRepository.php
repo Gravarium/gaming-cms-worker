e2c5fa47420aead9fb7702ef813aef62b4859785
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-24 12:28:23 +0200

    FCP-063: wire authorized private download delivery

---
+++ src/Repository/Download/DownloadVersionRepository.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace App\Repository\Download;
+use App\Entity\Download\DownloadPackage;
+use App\Entity\Download\DownloadVersion;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+/** @extends ServiceEntityRepository<DownloadVersion> */
+final class DownloadVersionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry){parent::__construct($registry,DownloadVersion::class);}
+    /** @return list<DownloadVersion> */ public function forPackage(DownloadPackage $package):array{return $this->findBy(['package'=>$package],['id'=>'DESC']);}
+}
