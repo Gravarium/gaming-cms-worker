20803f7b7367563249e196fe61fc462142d28916
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-26 13:17:29 +0200

    Test malformed locale preference boundaries

--- tests/Internationalization/LocalePolicyTest.php
+++ tests/Internationalization/LocalePolicyTest.php
@@ -14,6 +14,23 @@ final class LocalePolicyTest extends TestCase
         self::assertSame(['de', 'en'], (new LocalePolicy())->normalizeEnabled(['de', 'xx', 'en', 'de']));
     }
 
+    public function testIgnoresNonStringAndUnsupportedPersistedCandidates(): void
+    {
+        self::assertSame(
+            ['de', 'fr', 'en'],
+            (new LocalePolicy())->normalizeEnabled(['de', 7, null, ['en'], 'xx', 'fr', 'en', 'de']),
+        );
+    }
+
+    public function testBoundsCollectionShapeAndLocaleTagLength(): void
+    {
+        $policy = new LocalePolicy();
+
+        self::assertSame(['de'], $policy->normalizeEnabled(['primary' => 'en']));
+        self::assertSame(['de'], $policy->normalizeEnabled(array_fill(0, 65, 'en')));
+        self::assertSame(['en'], $policy->normalizeEnabled([str_repeat('x', 17), 'en']));
+    }
+
     public function testNeverReturnsAnUnsupportedLocale(): void
     {
         $policy = new LocalePolicy();
@@ -21,5 +38,6 @@ final class LocalePolicyTest extends TestCase
         self::assertSame('en', $policy->choose('xx', ['de', 'en'], 'en'));
         self::assertSame('de', $policy->choose('xx', ['de'], 'xx'));
         self::assertSame('de', $policy->choose('xx', [], 'xx'));
+        self::assertSame('en', $policy->choose('en', ['de', false, 'en'], 'de'));
     }
 }
