365b53e7838d9bb59e7ece22e0677b1598ae1a5c
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-26 18:53:12 +0200

    Expose content tag name limit in the form

--- src/Form/ContentTagType.php
+++ src/Form/ContentTagType.php
@@ -1,15 +1,29 @@
 <?php
 
 declare(strict_types=1);
+
 namespace App\Form;
+
 use App\Entity\ContentTag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
 /** @extends AbstractType<ContentTag> */
 final class ContentTagType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void { $builder->add('name', null, ['label' => 'Name'])->add('description', TextareaType::class, ['label' => 'Beschreibung', 'required' => false, 'attr' => ['rows' => 4, 'maxlength' => 500]]); }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Name',
+                'constraints' => [new Length(max: 100)],
+                'attr' => ['maxlength' => 100],
+            ])
+            ->add('description', TextareaType::class, ['label' => 'Beschreibung', 'required' => false, 'attr' => ['rows' => 4, 'maxlength' => 500]]);
+    }
+
     public function configureOptions(OptionsResolver $resolver): void { $resolver->setDefaults(['data_class' => ContentTag::class]); }
 }
