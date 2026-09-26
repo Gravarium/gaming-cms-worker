3297e301a667c2cdec965a96ee525dd9f34b3d04
GravariumDev <die_todeskrallen@yahoo.de>
2026-09-26 18:50:07 +0200

    Bound guild announcement title form input

--- src/Form/GuildAnnouncementType.php
+++ src/Form/GuildAnnouncementType.php
@@ -10,13 +10,25 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 /** @extends AbstractType<GuildAnnouncement> */
 final class GuildAnnouncementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('title', null, ['label' => 'Titel'])->add('body', TextareaType::class, ['label' => 'Mitteilung', 'attr' => ['rows' => 9]])->add('pinned', CheckboxType::class, ['label' => 'Oben anheften', 'required' => false]);
+        $builder
+            ->add('title', null, [
+                'label' => 'Titel',
+                'constraints' => [new Length(max: 180)],
+                'attr' => ['maxlength' => 180],
+            ])
+            ->add('body', TextareaType::class, ['label' => 'Mitteilung', 'attr' => ['rows' => 9]])
+            ->add('pinned', CheckboxType::class, ['label' => 'Oben anheften', 'required' => false]);
     }
-    public function configureOptions(OptionsResolver $resolver): void { $resolver->setDefaults(['data_class' => GuildAnnouncement::class]); }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => GuildAnnouncement::class]);
+    }
 }
